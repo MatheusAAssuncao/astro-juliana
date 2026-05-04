6ea6c862e8ca54c327b32d071a7cb8dd6155873f
Matheus Assunção <matheus.tba@hotmail.com>
2026-05-04 21:30:39 +0100

    Ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-juliana.matheusassuncao.com',
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
